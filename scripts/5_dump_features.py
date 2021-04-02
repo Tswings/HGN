cc6c7f0eb215b6ee4579d2f62d431dae95008d89
Tswings <39293946+Tswings@users.noreply.github.com>
2021-04-03 01:45:35 +1300

    Update 5_dump_features.py

--- scripts/5_dump_features.py
+++ scripts/5_dump_features.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from sys import path
+path.append("../")
+
 import argparse
 import json
 import gzip
